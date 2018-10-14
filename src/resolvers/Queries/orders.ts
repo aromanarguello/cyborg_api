c141703e57ec507208fe58e1b8ce267c341adf68
Alejandro <aromanarguello@gmail.com>
2018-10-14 11:53:35 -0400

    fetch order by order id

--- src/resolvers/Queries/orders.ts
+++ src/resolvers/Queries/orders.ts
@@ -1,5 +1,8 @@
 export const orderQueries = {
     async fetchOrders(_, {}, ctx, info) {
         return ctx.db.orders({}, info);
+    },
+    async fetchOrderById(_, { id }, ctx, info) {
+        return ctx.db.order({ id }, info);
     }
 };
