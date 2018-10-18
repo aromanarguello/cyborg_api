79676551550729b84abf675cd0c21f140f8da7bd
Alejandro <aromanarguello@gmail.com>
2018-10-18 18:14:44 -0400

    Modified add new order
    Made add new order to take item id array

--- src/resolvers/Mutations/orders.ts
+++ src/resolvers/Mutations/orders.ts
@@ -2,35 +2,37 @@ import { getUserId } from '../../utils';
 import { IContext } from '../Types/context';
 
 export const orderMutations = {
-    async newOrder(_, args, ctx, info) {
-        const { input: { item, price, provider, owner: { email } } } = args;
+    async newOrder(_, args, ctx: IContext) {
+        const { input: { itemId, price, provider, owner: { email } } } = args;
         const userId = getUserId(ctx);
         const user = await ctx.db.user({ id: userId });
 
         if (user.email !== email) throw new Error('User does not exist');
 
-        // to do fix create order to take an 
-        // array of items
         return ctx.db.createOrder({
-            item,
+            itemId: {
+                set: itemId
+            },
             price,
             provider,
             owner: {
                 connect: { email }
             }
-        }, info);
+        });
     },
-    async updateOrder(_, { input: { item, price, provider, id } }, ctx: IContext) {
+    async updateOrder(_, { input: { itemId, price, provider, id } }, ctx: IContext) {
         getUserId(ctx);
         return ctx.db.updateOrder({
             where: {
                 id
             },
             data: {
-                item,
+                itemId: {
+                    set: itemId
+                },
                 price,
                 provider
             }
-        })
+        });
     }
 };
